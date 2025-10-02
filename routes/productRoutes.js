c5545e40afbc48544d9a66ca9974bc4a03fe833a
Karen Suelen da Silva <karensusilva@gmail.com>
2025-10-02 17:33:01 -0300

    alterações

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,41 +1,77 @@
-const express = require('express');
+const express = require('express')
+const router = express.Router()
+const { v4: uuidv4 } = require('uuid')
 const fs = require('fs');
-const router = express.Router();
-const file = './data/product.json';
 
-router.get('/', (req, res) => {
-  const products = JSON.parse(fs.readFileSync(file));
-  res.json(products);
-});
+var productsDB = loadProducts();
+
+function loadProducts() {
+    try {
+      return JSON.parse(fs.readFileSync('./src/db/product.json', 'utf8'));
+    } catch (err) {
+      return [];
+    }
+}
+
+function saveProducts() {
+    try {
+      fs.writeFileSync('./src/db/product.json', JSON.stringify(productsDB, null, 2));
+      return "Saved"
+    } catch (err) {
+      return "Not saved";
+    }
+}
+
+router.get('/', (req, res) =>{
+    productsDB = loadProducts();
+    res.json(productsDB);
+})
 
 router.get('/:id', (req, res) => {
-  const products = JSON.parse(fs.readFileSync(file));
-  const product = products.find(p => p.id === req.params.id);
-  product ? res.json(product) : res.status(404).json({error: 'Produto não encontrado'});
-});
+    const id = req.params.id
+    productsDB = loadProducts();
+    var product = productsDB.find((product) => product.id === id )
+    if(!product) return res.status(404).json({
+        "erro": "Produto não encontrado!"
+    })
+    res.json(product)
+})
 
 router.post('/', (req, res) => {
-  const products = JSON.parse(fs.readFileSync(file));
-  const novo = { id: Date.now().toString(), ...req.body };
-  products.push(novo);
-  fs.writeFileSync(file, JSON.stringify(products, null, 2));
-  res.status(201).json(novo);
-});
+    const newProduct = {
+        id: uuidv4(),
+        ...req.body
+    }
+    productsDB = loadProducts();
+    productsDB.push(newProduct)
+    saveProducts();
+    return res.json(newProduct)
+})
 
 router.put('/:id', (req, res) => {
-  const products = JSON.parse(fs.readFileSync(file));
-  const index = products.findIndex(p => p.id === req.params.id);
-  if (index === -1) return res.status(404).json({error:'Produto não encontrado'});
-  products[index] = { ...products[index], ...req.body };
-  fs.writeFileSync(file, JSON.stringify(products, null, 2));
-  res.json(products[index]);
-});
+    const id = req.params.id
+    const newProduct = req.body
+    productsDB = loadProducts();
+    const currentIndex = productsDB.findIndex((product) => product.id === id )
+    if(currentIndex === -1) 
+        return res.status(404).json({
+        "erro": "Produto não encontrado!"
+    })
+    productsDB[currentIndex] = { id, ...newProduct }
+    saveProducts();
+    return res.json(productsDB[currentIndex])
+})
 
 router.delete('/:id', (req, res) => {
-  const products = JSON.parse(fs.readFileSync(file));
-  const novaLista = products.filter(p => p.id !== req.params.id);
-  fs.writeFileSync(file, JSON.stringify(novaLista, null, 2));
-  res.json({message: 'Produto removido'});
-});
+    const id = req.params.id
+    productsDB = loadProducts();
+    const currentIndex = productsDB.findIndex((product) => product.id === id )
+    if(currentIndex === -1) return res.status(404).json({
+        "erro": "Produto não encontrado!"
+    })
+    var deletado = productsDB.splice(currentIndex, 1)
+    saveProducts();
+    res.json(deletado)
+})
 
 module.exports = router;
--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -1,41 +1,76 @@
-const express = require('express');
+const express = require('express')
+const router = express.Router()
+const { v4: uuidv4 } = require('uuid')
 const fs = require('fs');
-const router = express.Router();
-const file = './data/store.json';
 
-router.get('/', (req, res) => {
-  const stores = JSON.parse(fs.readFileSync(file));
-  res.json(stores);
-});
+var storesDB = loadStores();
+
+function loadStores() {
+    try {
+      return JSON.parse(fs.readFileSync('./src/db/store.json', 'utf8'));
+    } catch (err) {
+      return [];
+    }
+}
+function saveStores() {
+    try {
+      fs.writeFileSync('./src/db/store.json', JSON.stringify(storesDB, null, 2));
+      return "Saved"
+    } catch (err) {
+      return "Not saved";
+    }
+}
+
+router.get('/', (req, res) =>{
+    storesDB = loadStores();
+    res.json(storesDB);
+})
 
 router.get('/:id', (req, res) => {
-  const stores = JSON.parse(fs.readFileSync(file));
-  const store = stores.find(s => s.id === req.params.id);
-  store ? res.json(store) : res.status(404).json({error: 'Loja não encontrada'});
-});
+    const id = req.params.id
+    storesDB = loadStores();
+    var store = storesDB.find((store) => store.id === id )
+    if(!store) return res.status(404).json({
+        "erro": "Loja não encontrada!"
+    })
+    res.json(store)
+})
 
 router.post('/', (req, res) => {
-  const stores = JSON.parse(fs.readFileSync(file));
-  const nova = { id: Date.now().toString(), ...req.body };
-  stores.push(nova);
-  fs.writeFileSync(file, JSON.stringify(stores, null, 2));
-  res.status(201).json(nova);
-});
+    const newStore = {
+        id: uuidv4(),
+        ...req.body
+    }
+    storesDB = loadStores();
+    storesDB.push(newStore)
+    saveStores();
+    return res.json(newStore)
+})
 
 router.put('/:id', (req, res) => {
-  const stores = JSON.parse(fs.readFileSync(file));
-  const index = stores.findIndex(s => s.id === req.params.id);
-  if (index === -1) return res.status(404).json({error:'Loja não encontrada'});
-  stores[index] = { ...stores[index], ...req.body };
-  fs.writeFileSync(file, JSON.stringify(stores, null, 2));
-  res.json(stores[index]);
-});
+    const id = req.params.id
+    const newStore = req.body
+    storesDB = loadStores();
+    const currentIndex = storesDB.findIndex((store) => store.id === id )
+    if(currentIndex === -1) 
+        return res.status(404).json({
+        "erro": "Loja não encontrada!"
+    })
+    storesDB[currentIndex] = { id, ...newStore }
+    saveStores();
+    return res.json(storesDB[currentIndex])
+})
 
 router.delete('/:id', (req, res) => {
-  let stores = JSON.parse(fs.readFileSync(file));
-  const novaLista = stores.filter(s => s.id !== req.params.id);
-  fs.writeFileSync(file, JSON.stringify(novaLista, null, 2));
-  res.json({message: 'Loja removida'});
-});
+    const id = req.params.id
+    storesDB = loadStores();
+    const currentIndex = storesDB.findIndex((store) => store.id === id )
+    if(currentIndex === -1) return res.status(404).json({
+        "erro": "Loja não encontrada!"
+    })
+    var deletado = storesDB.splice(currentIndex, 1)
+    saveStores();
+    res.json(deletado)
+})
 
 module.exports = router;
